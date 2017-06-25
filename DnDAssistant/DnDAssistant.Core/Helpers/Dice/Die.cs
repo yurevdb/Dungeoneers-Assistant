c09868af3531f1d8d621dd4b09f7e317019fcc17
WellTemperedFate <giovanni.allemeersch@gmail.com>
2017-06-25 18:02:21 +0200

    Made Dice class + Die enumerator for throwing dice. Also added Unit test for these classes.

---
+++ DnDAssistant/DnDAssistant.Core/Helpers/Dice/Die.cs
@@ -0,0 +1,21 @@
+﻿namespace DnDAssistant.Core
+{
+    /// <summary>
+    /// Enumerator for all the dice
+    /// </summary>
+    public enum Die
+    {
+        /// <summary>
+        /// 4-sided Die
+        /// </summary>
+        D4 = 4,
+        /// <summary>
+        /// 6-sided Die
+        /// </summary>
+        D6 = 6,
+        /// <summary>
+        /// 8-sided Die
+        /// </summary>
+        D8 = 8,
+    }
+}
