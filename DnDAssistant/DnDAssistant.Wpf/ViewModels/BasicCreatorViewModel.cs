eabb2a99192a60c3f457cc723061a2e1dca61297
WellTemperedFate <giovanni.allemeersch@gmail.com>
2017-09-08 16:58:02 +0200

    Added some basic databinding and properties for testing purposes

--- DnDAssistant/DnDAssistant.Wpf/ViewModels/BasicCreatorViewModel.cs
+++ DnDAssistant/DnDAssistant.Wpf/ViewModels/BasicCreatorViewModel.cs
@@ -8,9 +8,20 @@ namespace DnDAssistant.Wpf.ViewModels
 {
     class BasicCreatorViewModel
     {
-        public string Name { get; set; }
+#region Public properties
+        /// <summary>
+        /// <see cref="string"/> containing the chosen name of the character
+        /// </summary>
+        public string PlayerName { get; set; }
+        /// <summary>
+        /// <see cref="string"/> containing the picked class of the character
+        /// </summary>
         public string PickedClass{ get; set; }
+        /// <summary>
+        /// <see cref="string"/> containing the picked race of the character
+        /// </summary>
         public string PickedRace { get; set; }
+#endregion
 
 
     }
