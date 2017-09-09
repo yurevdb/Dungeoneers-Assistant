23ea22e452fb32967b87fed4b47e121988c81d7f
WellTemperedFate <giovanni.allemeersch@gmail.com>
2017-09-09 21:11:37 +0200

    Added a Racial Bonus Checker and added a testing unit for this class

--- DnDAssistant/DnDAssistant.UnitTester/BasicCharacterCreatorTester.cs
+++ DnDAssistant/DnDAssistant.UnitTester/BasicCharacterCreatorTester.cs
@@ -1,16 +1,35 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DnDAssistant.Wpf;
+using System.Collections.Generic;
 
 namespace DnDAssistant.UnitTester
 {
     [TestClass]
     public class CharacterCreatorBasicTester
     {
+        //[TestMethod]
+        //public void CheckDataBinding()
+        //{
+        //    var Model = new BasicCreatorViewModel() { PlayerName = "Wally" };
+
+        //}
+
         [TestMethod]
-        public void CheckDataBinding()
+        public void CheckSubRacePicker()
         {
-            var Model = new BasicCreatorViewModel() { PlayerName = "Wally"};
-            
+            var TestList = new List<string>();
+            var CompareList = new List<string>() { "Hill Dwarf", "Mountain Dwarf" };
+            TestList = RacialBonusChecker.SubRaceChecker("Dwarf");
+
+            Assert.AreEqual(CompareList, TestList);
+
+           
+            var CompareList2 = new List<string>() { "High Elf", "Wood Elf" };
+            TestList = RacialBonusChecker.SubRaceChecker("Elf");
+
+            Assert.AreEqual(CompareList2, TestList);
         }
+
+
     }
 }
