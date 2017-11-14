88f0d539d43633f4281a6a70bf2172ce326d83ba
Yuré Vanderbruggen <yurevdb@gmail.com>
2017-11-14 08:58:06 +0100

    Created the xamarin initial cross-platform project

---
+++ DnDAssistant/DnDAssistant.Xamarin/DnDAssistant.Xamarin/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace DnDAssistant.Xamarin.Views
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
